bc04a2c1bc3304580c221af8cdc57babb3e26f56
rschouten97 <r.schouten@tools4ever.com>
2021-05-25 16:19:20 +0200

    Added support for positions

--- persons.ps1
+++ persons.ps1
@@ -1,20 +1,21 @@
 $config = ConvertFrom-Json $configuration
 
 # ODBC connection string
-$connectionString = $config.connectionString;
+$connectionString = $($config.connectionString);
+$includePositions = $($config.switchIncludePositions)
 
-function Get-SQLData{
+function Get-SQLData {
     param(
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         $connectionString,
 
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         $SqlQuery,
 
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         [ref]$Data
     )
-    try{
+    try {
         $Data.value = $null
 
         # Initialize connection and execute query
@@ -38,21 +39,23 @@ function Get-SQLData{
         $null = $SqlAdapter.Fill($DataSet)
 
         # Set the output with returned data
-        $Data.value =  $DataSet.Tables[0] | Select-Object -Property * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors;
+        $Data.value = $DataSet.Tables[0] | Select-Object -Property * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors;
         Write-Verbose -Verbose "Successfully performed SQL query. Returned [$($DataSet.Tables[0].Columns.Count)] columns and [$($DataSet.Tables[0].Rows.Count)] rows"
         
-    } catch {
+    }
+    catch {
         $Data.Value = $null
         Write-Error $_
-    }finally{
-        if($SqlConnection.State -eq "Open"){
+    }
+    finally {
+        if ($SqlConnection.State -eq "Open") {
             $SqlConnection.close()
         }
-        Write-Verbose -Verbose "Successfully disconnected SQL Oracle database"
+        Write-Verbose -Verbose "Successfully disconnected from SQL database"
     }
 }
 
-try{
+try {
     # Get Department data
     $queryPersons = "SELECT 
         tpersoon.pers_nr,
@@ -71,14 +74,6 @@ try{
         tpersoon.tssnvgsl_kd, 
         tpersoon.burgst_kd, 
         tpersoon.geslacht, 
-        tpersoon.geb_dt, 
-        tpersoon.w_straat, 
-        tpersoon.w_huisnr,
-        tpersoon.w_hnr_toev, 
-        tpersoon.w_postcode, 
-        tpersoon.w_plaats, 
-        tpersoon.w_land_kd, 
-        tpersoon.w_tel_nr, 
         tpersoon.mobiel_tel_nr, 
         tpersoon.werk_tel_nr, 
         tpersoon.email_werk, 
@@ -94,7 +89,7 @@ try{
     $queryContracts = "SELECT
         tcontract.pers_nr,
         tcontract.dv_vlgnr,
-        topdrgvr.opdrgvr_nr,
+        tcontract.opdrgvr_nr,
         topdrgvr.opdrgvr_oms,
         tcontract.oe_hier_sl AS afdeling,
         tafdeling.oe_vol_nm,
@@ -115,15 +110,54 @@ try{
         LEFT JOIN dpib015 tafdeling ON tcontract.oe_hier_sl = tafdeling.dpib015_sl
         LEFT JOIN dpib004 tkostenplaats ON tafdeling.kstpl_kd = tkostenplaats.kstpl_kd
         LEFT JOIN dpic351 tfunctie ON tcontract.primfunc_kd = tfunctie.func_kd
-        LEFT JOIN dpic200 topdrgvr ON tcontract.OPDRGVR_NR = topdrgvr.OPDRGVR_NR 
+        LEFT JOIN dpic200 topdrgvr ON tcontract.OPDRGVR_NR = topdrgvr.OPDRGVR_NR
     "
     $contracts = New-Object System.Collections.ArrayList
     Get-SQLData -connectionString $connectionString -SqlQuery $queryContracts -Data ([ref]$contracts)
     Write-Verbose -Verbose "Contract query completed";
-    
+        
     # Group the contracts
     Write-Verbose "Grouping contracts..." -Verbose
-    $contracts = $contracts | Group-Object PERS_NR -AsHashTable
+    $contracts = $contracts | Group-Object PERS_NR -AsHashTable  
+    
+    if ($true -eq $includePositions) {
+        # SQL query for contracts
+        $queryPositions = "SELECT
+            tposition.pers_nr,
+            tposition.dv_vlgnr,
+            tcontract.opdrgvr_nr,
+            topdrgvr.opdrgvr_oms,
+            tposition.oe_oper_sl AS afdeling,
+            tafdeling.oe_vol_nm,
+            tafdeling.oe_kort_nm,
+            tafdeling.kstpl_kd,
+            tkostenplaats.kstpl_nm,
+            tafdeling.kstdrg_kd,
+            tfunctie.func_kd,
+            tfunctie.func_oms,
+            tfunctie.funtyp_kd,
+            tposition.ingang_dt as indnst_dt,  
+            tposition.eind_dt as uitdnst_dt,
+            tposition.uren_pw,
+	        tcontract.deelb_perc,
+            tcontract.arelsrt_kd
+        FROM
+	        dpic310 tposition
+	        INNER JOIN dpic300 tcontract ON tcontract.pers_nr = tposition.pers_nr
+		        AND tcontract.dv_vlgnr = tposition.dv_vlgnr
+            LEFT JOIN dpib015 tafdeling ON tposition.oe_oper_sl = tafdeling.dpib015_sl
+            LEFT JOIN dpib004 tkostenplaats ON tafdeling.kstpl_kd = tkostenplaats.kstpl_kd
+            LEFT JOIN dpic351 tfunctie ON tposition.operfunc_kd = tfunctie.func_kd
+	        LEFT JOIN dpic200 topdrgvr ON tcontract.OPDRGVR_NR = topdrgvr.OPDRGVR_NR
+        "
+        $positions = New-Object System.Collections.ArrayList
+        Get-SQLData -connectionString $connectionString -SqlQuery $queryPositions -Data ([ref]$positions)
+        Write-Verbose -Verbose "Position query completed";
+
+        # Group the positions
+        Write-Verbose "Grouping positions..." -Verbose
+        $positions = $positions | Group-Object PERS_NR -AsHashTable  
+    }
 
     # Extend the persons with positions and required fields
     Write-Verbose "Augmenting persons with required fields..." -Verbose
@@ -142,39 +176,46 @@ try{
 
     # Enhance person object
     Write-Verbose "Augmenting persons with contract data..." -Verbose
-    foreach($person in $persons){
+    $persons | ForEach-Object {
         # Add Contracts to person object
-        if(-not([string]::IsNullOrEmpty($person.ExternalId))){
-            $contractsPerson = $contracts[$person.ExternalId]
-            if ( -not($null -eq $contractsPerson) ){
-                $person.Contracts = $contractsPerson
+        if (-not([string]::IsNullOrEmpty($_.PERS_NR))) {
+            $contractsPerson = $contracts[$_.PERS_NR]
+            if ( -not($null -eq $contractsPerson) ) {
+                $_.Contracts = $contractsPerson
+            }
+
+            if ($true -eq $includePositions) {
+                $positionsPerson = $positions[$_.PERS_NR]
+                if ($null -ne $positionsPerson) {
+                    $_.Contracts += $positionsPerson
+                }
             }
         }
-
+        
         # Convert naming convention codes to standard
-        Switch($person.GBRK_NAAM ){
-            "E" {
-                $person.GBRK_NAAM = "B"
-            }
-            "P" {
-                $person.GBRK_NAAM = "P"
-            }
-            "C" {
-                $person.GBRK_NAAM = "BP"
-            }
-            "B" {
-                $person.GBRK_NAAM = "PB"
-            }
-            "D" {
-                $person.GBRK_NAAM = "BP"
-            }
+        if($_.GBRK_NAAM -eq "E") {
+            $_.GBRK_NAAM = "B"
+        }elseif ($_.GBRK_NAAM -eq "P") {
+            $_.GBRK_NAAM = "P"
+        }elseif ($_.GBRK_NAAM -eq "C") {
+            $_.GBRK_NAAM = "BP"
+        }elseif ($_.GBRK_NAAM -eq "B") {
+            $_.GBRK_NAAM = "PB"
+        }elseif ($_.GBRK_NAAM -eq "D") {
+            $_.GBRK_NAAM = "BP"
+        }else{
+            $_.GBRK_NAAM = "B"    
         }
-
-        # Export and sanitize the persons in json format
-        $json = $person | ConvertTo-Json -Depth 10
-        $json = $json.Replace("._", "__")
-        Write-Output $json
     }
-}catch{
+
+    # Filtering out persons without contracts
+    #Write-Verbose "Filtering out persons without contract data..." -Verbose
+    #$persons = $persons | Where-Object contracts -ne $null
+
+    # Export and sanitize the persons in json format
+    $json = $persons | ConvertTo-Json -Depth 10
+    $json = $json.Replace("._", "__")
+    Write-Output $json
+}catch {
     Write-Error $_
 }
